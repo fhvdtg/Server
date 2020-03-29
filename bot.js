1a969aaaa79641495d59c43d9f357c9dfeb4a8c8
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-03-30 00:03:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2594,4 +2594,177 @@ message.channel.send(embed)
 
 });
 
+client.on('message', msg => {
+  if(msg.content === 'Hello')
+  msg.reply('Hello! ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'Hi')
+  msg.reply('Hi! ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'Hey')
+  msg.reply('Hey ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'Back')
+  msg.reply('Welcome Back ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'IP')
+  msg.reply('IP: play.vortexprison.net ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'Bye')
+  msg.reply('Good Bye ❤ ')
+});
+ 
+client.on('message', message => {
+if(!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);if (command == "bc") {if(!message.member.roles.find('name','bc')) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**⛔ you must have \`ADMINISTRATOR\` premission**, or role called "bc"`)}
+let args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.channel.send(`**:rolling_eyes: please type the broadcast message**`)
+let filter = m => m.author.id == message.author.id
+let idx = 0, fails = 0;let broadcastt = new Discord.RichEmbed().setColor('#36393e')
+.addField(`**[1] broadcast for all members\n\n[2] broadcast for online members\n\n[3] broadcast for a specific role\n\n[4] broadcast with photo\n\n[0] to cansel**`,`** **`)
+.setDescription(`**Please type the number of your chose**`)
+.setFooter('you can add to the message [user] = mention the user')
+message.channel.send(broadcastt).then(msg => {
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']})
+.then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`**☑ Broadcast begin send....**`).then(m => {
+message.guild.members.map(member => {setTimeout(() => {member.send(args.replace('[user]',member).replace('[icon]',`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)).then(() => {}).catch((err) => {});},);});})}
+if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`**☑ Broadcast begin send....**`);
+message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {m.send(args.replace('[user]', m))})
+message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {m.send(args.replace('[user]', m)) })
+return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {m.send(args.replace('[user]', m)) })}
+if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`**Broadcast Has Been Canseled**`);}
+if(collected.first().content === '3') {msg.delete();message.channel.bulkDelete(1);
+message.channel.send('**Please Type the role name or id.**');
+message.channel.awaitMessages(filter, {max: 1,time: 40000,errors: ['time']}).then(t => {
+let R = t.first().content;
+let role = message.guild.roles.find('name',R) || message.guild.roles.get(R);
+if(!role) return message.channel.send('**😕 I Can\'t find this role please try again**'),msg.delete();
+message.channel.bulkDelete(2);
+if(role.members.size < 1) return message.channel.send('**there is no one have this role **😕');;
+let XYZ = new Discord.RichEmbed().setTitle('**:ballot_box_with_check: Broadcast begin send....**').setDescription(`**For the role: ${role}**`).setColor(role.color)
+message.channel.send(XYZ)
+message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {setTimeout(() => {n.send(args.replace('[user]',n)).catch((err) => {});});});}).catch(err =>{});}
+if(!collected.first().content.includes(['1','2','3','4','0'])) {msg.edit('Canceled.')}
+if(collected.first().content === '4') { msg.delete();
+message.channel.send('**✅ Please Type the photo link now**,Type "cansel" to cansel.').then(msgg =>{
+message.channel.awaitMessages(filter, {max: 1,time: 50000,errors: ['time']}).then(XX => {
+let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('**Broadcast Has Been Canseled**')}
+let embed = new Discord.RichEmbed().setImage(photo).setTitle(`**are you sure you want to send this? \`[y,n]\`**`).setColor('#36393e')
+message.channel.send(embed).catch(e =>{return message.channel.send('**The Photo link is wrong :x:**')});
+let filter = m => m.author.id == message.author.id
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
+let bc = new Discord.RichEmbed().setTitle(`${args}`).setImage(photo).setFooter(message.guild.name,`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)
+message.channel.bulkDelete(2);msgg.delete();message.channel.send('**☑ Broadcast begin send....**');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
+)})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('**Broadcast Has Been Canseled**')}
+})}).catch(myst =>{msgg.edit('Timed out.');})})
+}if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
+}).catch(mys =>{msg.edit('Timed out to chose.')})})}});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith("!say")) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You dont have** `ADMINISTRATOR` **permission**');
+var args = message.content.trim().split(/ +/g).slice(1);
+let cname = args[0];
+let chan = message.guild.channels.find(element => element.name === cname);
+if (chan) {
+    let text = args.slice(1).join(" ");
+    message.delete();
+    chan.send(text);
+} else {
+    let text = args.join(" ");
+    message.delete();
+    message.channel.send(text);
+   }
+}
+});
+
+client.on('message', message => {
+  if (message.content === prefix + 'cat') {
+    message.react(`🐱`)
+      message.channel.sendFile("http://thecatapi.com/api/images/get?format=src&type=png", "mystery-cat.png")
+  }
+});
+
+client.on('message', message => {
+  if (message.content.startsWith(prefix + 'sug')) {
+      if (message.author.bot) return
+      if (!message.guild) return message.reply('**:x: This Commands Just In Server**')
+      let Room = message.guild.channels.find(`name`, "「📊」suggestions")
+      if (!Room) return message.channel.send(`**Error** :octagonal_sign:\n**I Can't find the __suggestions__ Channel**`)
+      if(!Room.permissionsFor(client.user).has(['SEND_MESSAGES','READ_MESSAGES','EMBED_LINKS'])) return message.channel.send(`**Error** :octagonal_sign:
+I Don\'t have Permissions on tha suggestions channel`)
+
+      var args =  message.content.split(' ').slice(1).join(' ')
+      if (!args) return message.reply('**Please type your suggestion after the command**')
+      let embed = new Discord.RichEmbed()
+      .setColor('#311464')
+     .setAuthor(`New suggestion by ${message.author.username}`, 'https://media.discordapp.net/attachments/584630360017469461/584661803003281408/72-512.png?width=375&height=375')
+      .addField(`suggestion :`,`**${args}**`)
+      .setThumbnail(message.author.avatarURL)
+      .setTimestamp();
+      Room.sendEmbed(embed).then(c => {
+          c.react('👍').then(() =>
+              c.react('👎'))
+const sug = new Discord.RichEmbed()
+.setColor(0x00e2f3)
+.setAuthor(`Done your suggestion was sent`, 'https://media.discordapp.net/attachments/584630360017469461/584632506930823199/582246841186254869.png')
+message.channel.sendEmbed(sug)
+////
+})
+}
+});
+
+client.on('message', async message => {
+            if(message.content.includes('discord.gg')){
+                if(message.member.hasPermission("MANAGE_GUILD")) return;
+        if(!message.channel.guild) return;
+        message.delete()
+          var command = message.content.split(" ")[0];
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "Muted",
+          color: "#000000",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(muterole);
+    const embed500 = new Discord.RichEmbed()
+      .setTitle("Muted Ads")
+            .addField(`**  You Have Been Muted **` , `**Reason : Sharing Another Discord Link**`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} `)
+     message.channel.send(embed500)
+     message.author.send('` You are muted from VortexPrison server because you sent links in the chat! `');
+   
+       
+    }
+});
+
 client.login(process.env.BOT_TOKEN);//MrBloods bot
