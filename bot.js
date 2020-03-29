e8d33a252a4079948078649650341d4747c3bf4a
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-03-29 23:46:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -691,4 +691,194 @@ client.on('message', message => {
  }
  });
 
+
+client.on('message', message => {
+
+  var ms = require('ms')
+ 
+  var moment = require('moment');
+ 
+   
+  if (message.author.bot) return;
+ 
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+ 
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  let messageArray = message.content.split(" ");
+ 
+  let muteRole = message.guild.roles.find("name", "Muted");
+	
+  let embed = new Discord.RichEmbed()
+ 
+ .setImage("https://3.top4top.net/p_13403ntnj1.png")
+ 
+  if (command == "mute") {
+    
+  if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
+
+  if(!message.channel.guild) return message.reply('** This command only for servers**');
+          
+  if(!message.guild.member(message.author).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: You Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
+  if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: I Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
+  let user = message.mentions.users.first();
+ 
+  let Reason = message.content.split(" ").slice(4).join(" ");
+ 
+  let time = messageArray[2];
+ 
+  if (message.mentions.users.size < 1) return message.channel.sendEmbed(embed)
+   
+  if (!message.guild.member(user).bannable) return message.reply("**:x:I Don't Have Permission For Mute This User**");
+ 
+  if(!Reason)  {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+  }
+ 
+   if(!Reason && time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }  
+ 
+   if(!time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }
+   if(time) {
+    if(!time.match(/[1-60][s,m,h,d,w]/g))  return message.channel.send(':x: This Time Is Incorrect')
+
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+ 
+   }, ms(time));
+ 
+   }
+ 
+   if(time && Reason && user) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+	   
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+   
+   }, ms(time));
+ 
+   }
+ 
+   message.channel.send(`:white_check_mark: ${user} has been muted ! :zipper_mouth:`)
+ 
+   }
+ 
+});
+
+client.on('message',async message => {
+if(message.author.bot || message.channel.type === 'dm') return;
+let args = message.content.split(' ');
+let author = message.author.id;
+if(!credits[author]) credits[author] = {
+credits: 0
+}
+fs.writeFileSync("./credits.json", JSON.stringify(credits, null, 4));
+if(args[0].toLowerCase() == `${prefix}money`) {
+const mention = message.mentions.users.first() || message.author;
+const mentionn = message.mentions.users.first();
+if(!args[2]) {
+message.channel.send(`**${mention.username}, your 💵  balance is \`$${credits[mention.id].credits}\`**`)
+} else if(mentionn && args[2]) {
+if(isNaN(args[2])) return message.channel.send(`**:x: | Error**`);
+if(args[2] < 1) return message.channel.send(`**:x: | Error**`);
+if(mention.bot) return message.channel.send(`**:x: | Error**`);      
+if(mentionn.id === message.author.id) return message.channel.send(`**:x: | Error**`);
+if(args[2] > credits[author].credits) return message.channel.send(`**:x: | Error , You Don't Have Enough Credit**`);
+if(args[2].includes("-")) return message.channel.send(`**:x: | Error**`);
+let resulting = Math.floor(args[2]-(args[2]*(5/100)));
+let tax = Math.floor(args[2]*(5/100));
+let first = Math.floor(Math.random() * 9);
+let second = Math.floor(Math.random() * 9);
+let third = Math.floor(Math.random() * 9);
+let fourth = Math.floor(Math.random() * 9);
+let num = `${first}${second}${third}${fourth}`;
+let canvas = Canvas.createCanvas(108 , 40)
+let ctx = canvas.getContext('2d');
+const background = await Canvas.loadImage("https://cdn.discordapp.com/attachments/608278049091223552/617791172810899456/hmmm.png");
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+ctx.font = '20px Arial Bold';
+ctx.fontSize = '20px';
+ctx.fillStyle = "#ffffff";
+message.channel.send(`**${message.author.username}, Transfer Fees: \`${tax}\`, Amount: \`$${resulting.toLocaleString()}\`**
+type these numbers to confirm: `).then(essss => {
+ctx.fillText(num, canvas.width / 2.4, canvas.height / 1.7);
+message.channel.sendFile(canvas.toBuffer()).then(m => {
+message.channel.awaitMessages(r => r.author.id === message.author.id, { max: 1, time: 20000, errors:['time'] }).then(collected => {
+if(collected.first().content === num) {
+message.channel.send(`**:moneybag: | ${message.author.username}, Done Trans \`$${resulting.toLocaleString()}\` To ${mentionn}**`);
+mention.send(`**:money_with_wings: | Transfer Receipt \`\`\`You Have Received \`$${resulting.toLocaleString()}\` From User ${message.author.username}; (ID (${message.author.id})\`\`\``);
+m.delete();
+credits[author].credits += Math.floor((-resulting.toLocaleString()));
+credits[mentionn.id].credits += Math.floor((+resulting.toLocaleString()));
+fs.writeFileSync("./credits.json", JSON.stringify(credits, null, 4));
+} else {
+m.delete();
+essss.delete();
+}
+})
+})
+})
+}else {
+message.channel.send(`**:x: | Error , Please Command True Ex: \`${prefix}money [MentionUser] [Balance]\`**`);
+}
+}
+if(args[0].toLowerCase() === `${prefix}daily`) {
+let cooldown = 8.64e+7
+let Daily = time[message.author.id]
+if (Daily !== null && cooldown - (Date.now() - Daily) > 0) {
+let times = (cooldown - (Date.now() - Daily));
+message.channel.send(`**:stopwatch: |  ${message.author.username}, your daily :dollar: credits refreshes in ${pretty(times, {verbose:true})}.**`);
+fs.writeFile("./time.json", JSON.stringify(time), function(e) {
+if(e)throw e;
+})
+}else{
+let ammount = (300, 500, 100, 200, 120, 150, 350, 320, 220, 250);
+credits[author].credits += ammount;
+time[message.author.id] = Date.now();
+message.channel.send(`**:atm:  | ${message.author.username}, you received your 💰  ${ammount} daily credits!**`);
+fs.writeFile("./credits.json", JSON.stringify(credits), function(e) {
+if(e)throw e;
+})
+}
+}
+}); // By: FireKing , Codes
+
+client.on("message", message => {
+  var prefix = "!";
+  if(message.content.startsWith(prefix + "msg")) {
+    
+
+var color = message.content.split(" ")[1];
+  var text = message.content.split(" ").slice(2);
+    var tt = text.join(" ")
+  if(!color) return message.reply("You need to type the color !");
+    if(!tt) return message.reply("You need to type your message !");
+  let embed = new Discord.RichEmbed()
+  .setColor(color)
+  .setDescription(tt)
+  message.channel.send(embed).catch(Julian =>{console.log('`Error`: ' + Julian);
+message.channel.send("`Error`:" + Julian)
+    })
+  }
+  });
+
 client.login(process.env.BOT_TOKEN);//MrBloods bot
