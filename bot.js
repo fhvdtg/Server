0fee1ffa0f1b4c1e71792d485091b17a7b298f4c
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-03-29 23:25:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,4 +79,71 @@ console.log("1")
 console.log("====================================")
 });
 
+ client.on("guildMemberAdd", member => {
+  let welcomer = member.guild.channels.find("name","「👋」welcome");
+        if(!welcomer) return;
+        if(welcomer) {
+           moment.locale('ar-ly');
+           var h = member.user;
+          let norelden = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setThumbnail(h.avatarURL)
+          .setAuthor(h.username,h.avatarURL)
+          .addField(': When you joinned the discord: ',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+           .addField(': When you joinned the server: ',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true) 
+           .setFooter(`${h.tag}`,"https://media.discordapp.net/attachments/693389788023095306/693843294085447780/Vortex.png?width=902&height=508")
+       welcomer.send({embed:norelden});          
+                 
+   
+        }
+        });
+
+client.on('message', msg => {
+  if(msg.content === '!invite')
+  msg.reply('(Soon) :new_moon_with_face:')
+});
+
+ client.on('message',async message => {
+  let args = message.content.split(" ").slice(1).join(" ");
+  let role = message.guild.roles.find('name',args) || message.guild.roles.get(args);
+
+
+  if(message.content.startsWith(prefix + "rl")) {
+    if(!args) return message.reply('Type the rank name');
+    if(!role) return message.reply('This rank does not exist');
+    let iQp = new Discord.RichEmbed()
+    .setAuthor(message.author.tag,message.author.avatarURL)
+    .setTitle(message.guild.name)
+    .setThumbnail(message.guild.iconURL)
+    .addField('- Rank name',role.name,true)
+    .addField('- Rank id',role.id,true)
+    .addField('- Rank created',role.createdAt.toLocaleString(),true)
+    .addField('- Rank color',role.hexColor,true)
+    .addField('- Number of members with the same rank',role.members.size,true)
+    .addField('- Center rank between all ranks',role.position - message.guild.roles.size,true)
+    .addField('- Rank properties',role.permissions,true)
+    .setFooter(message.author.tag,message.author.avatarURL);
+
+    message.channel.send(iQp);
+  }
+});
+
+client.on("message", msg => {
+    var prefix = '!'//البركفس
+    if(msg.content.startsWith(prefix + 'myserver')){
+      let embed = new Discord.RichEmbed()
+      .setThumbnail(msg.guild.iconURL)
+      .setColor("RANDOM")
+      .addField("Year📆",msg.guild.createdAt.getFullYear())
+      .addField("Hour📆", msg.guild.createdAt.getHours())
+      .addField("Day📆", msg.guild.createdAt.getDay())
+      .addField("Month📆", msg.guild.createdAt.getMonth())
+      .addField("Minutes📆", msg.guild.createdAt.getMinutes())
+      .addField("Seconds📆", msg.guild.createdAt.getSeconds())
+      .addField("Full📆", msg.guild.createdAt.toLocaleString())
+      .setTimestamp()
+      msg.channel.send(embed);
+    }
+  });
+
 client.login(process.env.BOT_TOKEN);//MrBloods bot
