fb10a9b2bc89954eb334dbc0e63561f19eacb0ff
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-10-30 14:35:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,6 +87,85 @@ if(message.content.startsWith('!suggestion'))  {
     }
   });
 
+client.on('guildMemberAdd', msg => { // Commands Go Inside The client.on('message', 
+msg => )
+msg.guild.channels.get('771710273455259649').send({embed: {
+color: 3447003,
+author: {
+  name: client.user.username,
+  icon_url: client.user.avatarURL
+},
+title: "Welcome To ()!",
+url: "https://districtservices.net",
+description: "@MEMBER",
+fields: [{
+    name: "Fields",
+    value: "They can have different fields with small headlines."
+  },
+  {
+    name: "Masked links",
+    value: "You can put [masked links](http://google.com) inside of rich embeds."
+  },
+  {
+    name: "Markdown",
+    value: "You can put all the *usual* **__Markdown__** inside of them."
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© Example"
+}
+}}); });
+
+//Welcome & goodbye messages\\
+client.on('guildMemberAdd', member => {
+    member.roles.add(member.guild.roles.cache.find(i => i.name === 'Prison'))
+
+    const welcomeEmbed = new Discord.MessageEmbed()
+
+    welcomeEmbed.setColor('#5cf000')
+    welcomeEmbed.setTitle('**' + member.user.username + '** is now Among Us other **' + member.guild.memberCount + '** people')
+    welcomeEmbed.setImage('https://cdn.mos.cms.futurecdn.net/93GAa4wm3z4HbenzLbxWeQ-650-80.jpg.webp')
+
+    member.guild.channels.cache.find(i => i.name === 'welcome').send(welcomeEmbed)
+})
+
+client.on('guildMemberRemove', member => {
+    const goodbyeEmbed = new Discord.MessageEmbed()
+
+    goodbyeEmbed.setColor('#f00000')
+    goodbyeEmbed.setTitle('**' + member.user.username + '** was not the impostor there are **' + member.guild.memberCount + '** left Among Us')
+    goodbyeEmbed.setImage('https://gamewith-en.akamaized.net/article/thumbnail/rectangle/22183.png')
+
+    member.guild.channels.cache.find(i => i.name === 'welcome').send(goodbyeEmbed)
+})
+//Welcome & goodbye messages end\\
+
+client.on('guildMemberAdd', member => {
+client.on('message', 
+
+
+var role = member.guild.roles.find('name', 'Prisoner'); // Variable to get channel ID
+member.addRole(role); // Adds the default role to members
+
+member.guild.channels.get('771710273455259649').send({embed: {
+color: 3447003,
+title: "**SERVER NAME** Welcome Bot!",
+url: "WEBSITE URL",
+description: "Welcome *" + member + "* to the **Server name** discord server!",
+fields: [{
+    name: "Information",
+    value: "Some info on the server"
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© NAME OF SERVER 2018 - 2019"
+}
+}}); });
+
 client.on('message', message => {
     var prefix = "!";
     if(message.content === (prefix + "apply")) {
@@ -296,12 +375,6 @@ Channel: ${thisMessage}\`\`\`
     }
 });
 
-client.on('guildMemberAdd', member => {
-  console.log('User' + member.user.tag + 'has joined the server!');
-
-  var role = member.guild.roles.find('name', 'Prisoner');
-  member.addRole(role);
-});
 
 
 
